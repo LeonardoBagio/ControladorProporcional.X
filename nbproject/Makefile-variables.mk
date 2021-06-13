46d17ff933069260f4a732ccb626fab0f8ee2d71
Leonardo Bagio <49789651+LeonardoBagio@users.noreply.github.com>
2021-06-12 23:34:32 -0300

    Initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ControladorProporcional.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ControladorProporcional.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=controladorproporcional.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/controladorproporcional.x.tar
